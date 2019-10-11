9e4e6d105326732817121d3d46d9b4a831440da5
Mert <mertala@icloud.com>
2019-10-11 16:21:14 +0300

    Update ContentView.swift

--- Darkmode-SwiftUI/ContentView.swift
+++ Darkmode-SwiftUI/ContentView.swift
@@ -1,21 +1,20 @@
-//
-//  ContentView.swift
-//  Darkmode-SwiftUI
-//
-//  Created by Mert Ala on 9.10.2019.
-//  Copyright © 2019 Mert Ala. All rights reserved.
-//
 
 import SwiftUI
-
 struct ContentView: View {
+    
     var body: some View {
         Text("Hello World")
     }
 }
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      Group {
+         ContentView()
+            .environment(\.colorScheme, .light)
+         ContentView()
+            .environment(\.colorScheme, .dark)
+      }
+   }
 }
+#endif
